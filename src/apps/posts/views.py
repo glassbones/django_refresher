0eea024f0ab3295a135706f5cdb133db4a6e201a
glassbones <glassbonesofficial@gmail.com>
2020-12-15 08:54:12 -0600

    front end html for posts started

--- src/apps/posts/views.py
+++ src/apps/posts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from apps.posts.models import Post, Like
+from apps.profiles.models import Profile
 
 # Create your views here.
+def post_comment_create_and_list_view(request):
+    query_set = Post.objects.all()
+    profile = Profile.objects.get(user=request.user)
+
+    context = {
+        'query_set' : query_set,
+        'profile': profile
+    }
+
+    return render(request, 'posts/main.html', context)
+
+def like_toggle_post(request):
+    user = request.user
+    print(request.method)
+    if request.method == 'POST':
+        print('-----')
+        print(request.POST.get('post_id'))
+
+        print('-----')
+        post_id = request.POST.get('post_id')
+        post_obj = Post.objects.get(id=post_id)
+        profile = Profile.objects.get(user=user)
+
+        if profile in post_obj.liked.all():
+            post_obj.liked.remove(profile)
+        else:
+            post_obj.liked.add(profile)
+        
+        like, created = Like.objects.get_or_create(user=profile, post_id=post_id)
+
+        if not created:
+            if like.value == 'Like':
+                like.value='Unlike'
+            else:
+                like.value='Like'
+        else:
+            like.value='Like'
+        
+        post_obj.save()
+        like.save()
+    
+    return redirect('posts:main-post-view')
