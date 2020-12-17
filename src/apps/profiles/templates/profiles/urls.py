e0fbf2237eed36f9d5464ae824d893632de51179
glassbones <glassbonesofficial@gmail.com>
2020-12-17 06:56:43 -0600

    developing friends request html and functionality

--- src/apps/profiles/templates/profiles/urls.py
+++ src/apps/profiles/templates/profiles/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from apps.profiles.views import my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view
+from apps.profiles.views import (
+    my_profile_view,
+    invites_received_view,
+    ProfileListView,
+    profiles_list_view,
+    invite_profiles_list_view, 
+)
 
 app_name ='profiles'
 
 urlpatterns = [
     path('myprofile/', my_profile_view, name='my-profile-view'),
     path('myinvites/', invites_received_view, name='my-invites-view'),
-    path('all-profiles/', profiles_list_view, name='all-profiles-view'),
+    # path('all-profiles/', profiles_list_view, name='all-profiles-view'), #function path
+    path('all-profiles/', ProfileListView.as_view(), name='all-profiles-view'), #Class path
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
 ]
--- src/apps/profiles/views.py
+++ src/apps/profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from apps.profiles.models import Profile, Relationship
 from .forms import ProfileModelForm
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 # Create your views here.
 def my_profile_view(request):
     profile = Profile.objects.get(user=request.user)
@@ -27,16 +29,61 @@ def invites_received_view(request):
     context = {'query_set': query_set}
     return render(request,'profiles/my_invites.html', context)
 
-def profiles_list_view(request):
-    user = request.user
-    query_set = Profile.objects.get_all_profiles(user)
-
-    context = {'query_set': query_set}
-    return render(request,'profiles/profile_list.html', context)
-
 def invite_profiles_list_view(request):
     user = request.user
     query_set = Profile.objects.get_available_relationships(user)
 
     context = {'query_set': query_set}
     return render(request,'profiles/to_invite_profile_list.html', context)
+
+# same as ProfileListView just a function instead of a class
+# (start) this code is not being used
+def profiles_list_view(request):
+    user = request.user
+    query_set = Profile.objects.get_all_profiles(user)
+    context = {'query_set': query_set}
+    return render(request,'profiles/profile_list.html', context)
+# (end) this code is not being used
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profile_list.html'
+    #overiding attr name
+    context_object_name = 'query_set'
+
+    #overiding default method
+    def get_queryset(self):
+        query_set = Profile.objects.get_all_profiles(self.request.user)
+        return query_set
+
+    #overiding default method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get( username__iexact=self.request.user ) # "username__iexact" helps prevent errors?
+        profile = Profile.objects.get(user=user)
+        context['profile'] = profile
+
+        # grabing all relationships involving the user
+        relations_received = Relationship.objects.filter(sender=profile)
+        relations_sent = Relationship.objects.filter(receiver=profile)
+
+        # turning those lists of relationships into lists of users
+        received_rel_users = []
+        sent_rel_users = []
+
+        for rel in relations_received:
+            received_rel_users.append(rel.receiver.user)
+
+        for rel in relations_sent:
+            sent_rel_users.append(rel.sender.user)
+
+        context['received_rel_users'] = received_rel_users
+        context['sent_rel_users'] = sent_rel_users
+        context['is_empty'] = False
+
+        if len(self.get_queryset()) == 0:
+            context['is_empty'] = True
+
+        return context
+    
+    
